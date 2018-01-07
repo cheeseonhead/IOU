c5f38903784787fff7260cef201d10cc0284d198
Jeff Woo <cheeseonhead@gmail.com>
2018-01-07 16:45:50 -0500

    Created the project

---
+++ IOU/IOU/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IOU
+//
+//  Created by Cheese Onhead on 1/7/18.
+//  Copyright © 2018 okAyStudios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
