9f236d3a75eef540e249041a9ba9ac6161fecc15
Jeff Woo <cheeseonhead@gmail.com>
2018-01-07 19:44:03 -0500

    Added a bridging header

---
+++ IOU/IOU/IOU-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  IOU-Bridging-Header.h
+//  IOU
+//
+//  Created by Cheese Onhead on 1/7/18.
+//  Copyright © 2018 okAyStudios. All rights reserved.
+//
+
+#ifndef IOU_Bridging_Header_h
+#define IOU_Bridging_Header_h
+
+#import <Google/SignIn.h>
+
+#endif /* IOU_Bridging_Header_h */
