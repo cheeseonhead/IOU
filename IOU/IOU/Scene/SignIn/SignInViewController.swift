19ed8be0d316d87df87793a918dc6027927638a8
Jeff Woo <cheeseonhead@gmail.com>
2018-01-13 17:37:27 -0500

    Added a file

---
+++ IOU/IOU/Scene/SignIn/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  IOU
+//
+//  Created by Cheese Onhead on 1/13/18.
+//  Copyright © 2018 okAyStudios. All rights reserved.
+//
+
+import Foundation
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    
+}
