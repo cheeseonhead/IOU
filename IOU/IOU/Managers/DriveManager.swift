21b96e3f3510090265fef7d025bc0fcdb4d04af1
Jeff Woo <cheeseonhead@gmail.com>
2018-01-13 17:24:58 -0500

    Moved some code

--- IOU/IOU/Managers/DriveManager.swift
+++ IOU/IOU/Managers/DriveManager.swift
@@ -44,3 +44,68 @@ class DriveManager {
         }
     }
 }
+
+//
+//    func createFile() {
+//
+//        let file = GTLRDrive_File()
+//        file.name = "Testing"
+//        file.mimeType = "application/vnd.google-apps.spreadsheet"
+//
+//
+//        let query = GTLRDriveQuery_FilesCreate.query(withObject: file, uploadParameters: nil)
+//        service.executeQuery(query, delegate: self, didFinish: #selector(displayCreateResultWithTicket(ticket:finishedWithObject:error:)))
+//    }
+//
+//    @objc func displayCreateResultWithTicket(ticket: GTLRServiceTicket,
+//                                       finishedWithObject result: GTLRDrive_File,
+//                                       error: NSError?) {
+//        guard error == nil else {
+//            print("Error: \(error?.localizedDescription)")
+//            return
+//        }
+//
+//        print("File has been created: \(result.name)")
+//    }
+//
+//    func listFiles() {
+//        let query = GTLRDriveQuery_FilesList.query()
+//        query.corpora = "user"
+//        query.pageSize = 10
+//        service.executeQuery(query,
+//                             delegate: self,
+//                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:))
+//        )
+//    }
+//
+//    // Process the response and display output
+//    @objc func displayResultWithTicket(ticket: GTLRServiceTicket,
+//                                 finishedWithObject result : GTLRDrive_FileList,
+//                                 error : NSError?) {
+//
+//        if let error = error {
+//            print("Error: \(error.localizedDescription)")
+//            return
+//        }
+//
+//        var text = "";
+//        if let files = result.files, !files.isEmpty {
+//            text += "Files:\n"
+//            for file in files {
+//                text += "\(file.name!) (\(file.identifier!))\n"
+//            }
+//        } else {
+//            text += "No files found."
+//        }
+//
+//        print(text)
+//    }
+//
+//    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+//        guard error == nil else {
+//            print("\(error.localizedDescription)")
+//            return
+//        }
+//
+//        print("Has disconnected: \(user.profile.name)")
+//    }
