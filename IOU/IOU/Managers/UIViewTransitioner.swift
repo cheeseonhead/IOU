9083d279856debeadf85bbbe6df108b12b52a813
Jeff Woo <cheeseonhead@gmail.com>
2018-01-13 18:02:40 -0500

    Added some new files

---
+++ IOU/IOU/Managers/UIViewTransitioner.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewTransitioner.swift
+//  IOU
+//
+//  Created by Cheese Onhead on 1/13/18.
+//  Copyright © 2018 okAyStudios. All rights reserved.
+//
+
+import Foundation
---
+++ IOU/IOU/Shared/UIStoryboard/UIStoryboard+Instantiate.swift
@@ -0,0 +1,9 @@
+//
+//  UIStoryboard+Instantiate.swift
+//  IOU
+//
+//  Created by Cheese Onhead on 1/13/18.
+//  Copyright © 2018 okAyStudios. All rights reserved.
+//
+
+import Foundation
